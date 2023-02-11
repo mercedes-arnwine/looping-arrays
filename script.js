d298b4e78aa07b4508ed34fce50792b11404830a
mercedes-arnwine <80288739+mercedes-arnwine@users.noreply.github.com>
2023-02-11 08:47:54 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var programmingLanguages = [
+  "Scala",
+  "Java",
+  "Go",
+  "Swift",
+  "Cobol",
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "Elm",
+  "Rust",
+  "C#"
+];
+
+console.log(programmingLanguages);
+
+for (var language of programmingLanguages) {
+  console.log(`I want to learn ${language}!`);
+}
+// use forEach for array, make all items upper case
+// log out index starting at '1' with the uppercase element
+programmingLanguages.forEach(function (item, index) {
+  console.log(`${index + 1}. ${item.toUpperCase()}`);
+});
+
+// User .filter() to return a new array called updatedLangages
+// return languages without the letter "y"
+var updatedLanguages = programmingLanguages.filter(function (language) {
+  return language.includes("y");
+});
